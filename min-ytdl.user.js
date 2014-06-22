660172835c6dd5fb7e9ba9ca906fbbd1b162f74f
Stuart Baker <sdb@stuartdb.com>
2014-06-22 15:57:07 +1200

    Remove meta file and update metadata block

--- min-ytdl.user.js
+++ min-ytdl.user.js
@@ -1,12 +1,11 @@
 // ==UserScript==
 // @name min-ytdl
-// @version 0.1
-// @namespace https://github.com/stuartdb/min-ytdl
+// @version 0.2
+// @namespace http://stuartdb.com
 // @author Stuart Baker
 // @description Places a youtube-dl command with video link on youtube video pages
 // @include *://www.youtube.com/watch*
 // @downloadURL https://github.com/stuartdb/min-ytdl/raw/master/min-ytdl.user.js
-// @updateURL https://github.com/stuartdb/min-ytdl/raw/master/min-ytdl.meta.js
 // @run-at document-end
 // ==/UserScript==
 function get_url() {
